28ce8eecc9f90b23673b4e4db06b0eb695c95c21
ricardo <rictany@yahoo.com>
2024-02-24 23:29:41 -0500

    version

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "server1" {
+  ami = "ami-026b57f3c383c2eec"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform server"
+    Team = "DevOps"
+    env = "dev"
+  }
+  user_data = file("install.sh")
+}
